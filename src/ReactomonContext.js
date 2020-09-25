e5e397678cbb5c19916ec2daa9b50497200552dd
Igaunian <katieichenbaum@gmail.com>
2020-09-25 04:04:56 +0200

    add sprites to cards

--- src/ReactomonContext.js
+++ src/ReactomonContext.js
@@ -4,13 +4,15 @@ import axios from 'axios';
 
 export const ReactomonContext = createContext();
 
+
 export const PokemonListProvider = props => {
-    const [pokemons, setPokemons] = useState([{"name":"bulbasaur","url":"https://pokeapi.co/api/v2/pokemon/1/"},{"name":"ivysaur","url":"https://pokeapi.co/api/v2/pokemon/2/"},{"name":"venusaur","url":"https://pokeapi.co/api/v2/pokemon/3/"}]);
-    
+    // const [pokemons, setPokemons] = useState([{"name":"bulbasaur","url":"https://pokeapi.co/api/v2/pokemon/1/"},{"name":"ivysaur","url":"https://pokeapi.co/api/v2/pokemon/2/"},{"name":"venusaur","url":"https://pokeapi.co/api/v2/pokemon/3/"}]);
+    const [pokemons, setPokemons] = useState([]);
+
     useEffect(() => {
-        axios.get('https://pokeapi.co/api/v2/pokemon/?limit=50')
+        axios.get('https://pokeapi.co/api/v2/pokemon/?limit=100')
           .then(res => setPokemons(res.data.results));
-      }, [])
+    }, [])
 
     return (
         <ReactomonContext.Provider value={[pokemons, setPokemons]}>
@@ -20,8 +22,9 @@ export const PokemonListProvider = props => {
 }
 
 export const TypeListProvider = props => {
-    const [types, setTypes] = useState([{"name":"normal","url":"https://pokeapi.co/api/v2/type/1/"},{"name":"fighting","url":"https://pokeapi.co/api/v2/type/2/"},{"name":"flying","url":"https://pokeapi.co/api/v2/type/3/"}]);
-    
+    // const [types, setTypes] = useState([{"name":"normal","url":"https://pokeapi.co/api/v2/type/1/"},{"name":"fighting","url":"https://pokeapi.co/api/v2/type/2/"},{"name":"flying","url":"https://pokeapi.co/api/v2/type/3/"}]);
+    const [types, setTypes] = useState([]);
+
     useEffect(() => {
         axios.get('https://pokeapi.co/api/v2/type')
           .then(res => setTypes(res.data.results));
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -5,6 +5,7 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import { ReactomonContext } from '../ReactomonContext';
 
+
 const useStyles = makeStyles((theme) => ({
     root: {
       flexGrow: 1,
@@ -28,12 +29,8 @@ const linkStyle = {
 function PokemonList(props) {
     const [spacing, setSpacing] = React.useState(2);
     const classes = useStyles();
-    const [pokemons, setPokemons] = useContext(ReactomonContext);
-
-    const handleChange = (event) => {
-        setSpacing(Number(event.target.value));
-    };
-
+    const [pokemons, setPokemons, sprites, setSprites] = useContext(ReactomonContext);
+    
     return (
         <>
             <p></p>
@@ -41,8 +38,13 @@ function PokemonList(props) {
                 <Grid item xs={12}>
                     <Grid container justify="center" spacing={spacing}>
                         {pokemons.map((pokemon, i) => (
-                            <Grid item>
-                                <Link style={linkStyle} to={`/pokemon/${i + 1}`}><Paper className={classes.paper}>{pokemon.name}</Paper></Link>
+                            <Grid key={i} item>
+                                <Link style={linkStyle} to={`/pokemon/${i + 1}`}>
+                                    <Paper key={i} className={classes.paper}>
+                                        {pokemon.name}
+                                        <img style={{width: 80, height: 80, mode: 'fit'}} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i + 1}.png`}/>
+                                    </Paper>
+                                </Link>
                             </Grid>
                         ))}
                     </Grid>
